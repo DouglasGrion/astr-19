f93058eb41b1fa8c6f640a91dfcf0e4a7ea8e8d9
DouglasGrion <df2629@columbia.edu>
2026-03-31 13:06:01 -0700

    Uploads hello-world.py
    Uploaded python file to add-file branch. Now the repository is not empty

---
+++ hello-world.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 31 10:52:58 2026
+
+@author: douglasgrionfilho
+"""
+
+def PrintHelloWorld():
+    #this function prints the words Hello World!
+    
+    print("Hello Doug!")
+    
+def main():
+    #this is the main function of the program
+    
+    PrintHelloWorld()
+    
+
+if __name__ == "__main__":
+    main()
